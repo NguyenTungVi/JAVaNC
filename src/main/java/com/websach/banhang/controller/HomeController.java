8d61f5d0b51f4b88a8d028b8f42a07419e2f28b6
NTVinh04 <ngothanhvinh21104@gmail.com>
2025-11-17 03:09:37 +0700

    Update Home

--- src/main/java/com/websach/banhang/controller/HomeController.java
+++ src/main/java/com/websach/banhang/controller/HomeController.java
@@ -1,15 +1,32 @@
 package com.websach.banhang.controller;
 
+import com.websach.banhang.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Map;
 
 
 @Controller
 public class HomeController {
+    @Autowired
+    private OrderService orderService;
+
     @GetMapping("/home")
-    public String index() {
-        // Đảm bảo rằng trang chủ thực sự là home.html
-        return "home"; // hoặc "index" tùy thuộc vào tên file của bạn
+    public String home(Model model) {
+        // Lấy Top 10 cho trang home
+        List<Map<String, Object>> bestSellers = orderService.getTop10BestSellers();
+
+        // Nếu muốn chỉ hiển thị 6 sản phẩm ở home, giới hạn lại
+        if (bestSellers.size() > 4) {
+            bestSellers = bestSellers.subList(0, 4);
+        }
+        // Sử dụng cùng logic như trang best-sellers
+        model.addAttribute("bestSellers", orderService.getTop10BestSellers());
+        return "home";
     }
 
 }
--- src/main/java/com/websach/banhang/controller/IndexController.java
+++ src/main/java/com/websach/banhang/controller/IndexController.java
@@ -1,14 +1,32 @@
 package com.websach.banhang.controller;
 
+import com.websach.banhang.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Map;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    private OrderService orderService;
+
     @GetMapping("/")
-    public String index() {
-        return "index";
+    public String home(Model model) {
+        // Lấy Top 10 cho trang home
+        List<Map<String, Object>> bestSellers = orderService.getTop10BestSellers();
+
+        // Nếu muốn chỉ hiển thị 6 sản phẩm ở home, giới hạn lại
+        if (bestSellers.size() > 4) {
+            bestSellers = bestSellers.subList(0, 4);
+        }
+        // Sử dụng cùng logic như trang best-sellers
+        model.addAttribute("bestSellers", orderService.getTop10BestSellers());
+        return "home";
     }
 
     @GetMapping("/signin")
@@ -25,5 +43,4 @@ public class IndexController {
     public String about() {
         return "about";
     }
-
 }
